f4c3388ae17a02e7cbab2500c8c2e7f98f7f20fb
NicoOhler <32089075+NicoOhler@users.noreply.github.com>
2022-09-13 19:50:13 +0200

    Upload logos

--- api/config.py
+++ api/config.py
@@ -1,5 +1,5 @@
 REDIS_URL_FILE = "api/redis_url.key"  # file with redis url
-#REDIS_URL_FILE = "redis_url.key"  # file with redis url
+REDIS_URL_FILE = "redis_url.key"  # file with redis url
 with open(REDIS_URL_FILE) as file:  # read redis url from file
     REDIS_URL = file.read()
 
@@ -8,6 +8,3 @@ SKIP_DEFAULT = 0  # skips first 0 tweets for /timeline
 LIMIT_DEFAULT = 10  # limits to 10 tweets for /timeline
 LIMIT_OFFSET = 1  # offset for /timeline because redis is 0-indexed?
 TTL = 60 * 60 * 24 * 7  # expire after 7 days
-
-
-
