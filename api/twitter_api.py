69e58fa8c0b9ccb87e1a78d93f693630e5cb6203
NicoOhler <32089075+NicoOhler@users.noreply.github.com>
2022-09-13 18:07:10 +0200

    Improved tweet schema redundancy timelines now store tweet id instead of whole tweet each hashtag now has list of tweets using it each user now has list of their mentions

--- api/twitter_api.py
+++ api/twitter_api.py
@@ -34,86 +34,108 @@ app = FastAPI()
 # todo user profile
 # todo search tweets - GET /search/{query}
 
-# sample tweet
-tweet = {
-    "id": "86d65a60-3c13-4980-8255-84715e1f6b6e",
-    "from": "aigner",
-    "sent": 1662806699.5619178,
-    "content": "waduttn @riedl #waduttn",
-    "likes": 0,
-    "retweets": 0,
-    "mentions": ["riedl"],
-    "hashtags": ["#waduttn"],
-    "recipients": ["riedl", "kurz"],
-}
+
+def extract_hashtags(text):
+    return list(set([word[1:] for word in text.split() if word.startswith("#")]))
 
 
-async def add_tweet_to_hashtags(hashtags, tweet_id):
-    for hashtag in hashtags:
-        rdb.sadd(f"hashtag:{hashtag}", tweet_id)
+def extract_mentions(text):
+    return list(set([word[1:] for word in text.split() if word.startswith("@")]))
 
 
-async def add_tweet_to_mentions(mentions, tweet_id):
-    for mention in mentions:
-        rdb.sadd(f"mention:{mention}", tweet_id)
+def prepare_tweet(tweet, user):
+    tweet["id"] = str(uuid.uuid4())
+    tweet["from"] = user
+    tweet["time"] = time.time()
+    tweet["likes"] = 0
+    tweet["retweets"] = 0
+
+    # extract unique hashtags and mentions
+    tweet["hashtags"] = extract_hashtags(tweet["content"])
+    tweet["mentions"] = extract_mentions(tweet["content"])
+
+    # remove mentions of users who do not exist
+    for mention in tweet["mentions"]:
+        if not rdb.get(f"profile:{user}"):
+            tweet["mentions"].remove(mention)
+
+    # recipients are unique followers and mentioned users
+    tweet["recipients"] = list(set(tweet["mentions"] + rdb.smembers(f"followers:{user}")))
 
 
 # API endpoints
 # send tweet
 @app.post("/tweet/{user}")
 async def send_tweet(user: str, tweet: Request):
-    tweet = await tweet.json()
-    tweet["id"] = str(uuid.uuid4())
-    tweet["from"] = user
-    tweet["sent"] = time.time()
-    tweet = json.dumps(tweet)
 
-    # start transaction
-    pipe = rdb.pipeline(transaction=True)
-    pipe.multi()
+    tweet = prepare_tweet(await tweet.json(), user)
+    tweet_id = tweet["id"]
+
+    # todo start transaction
+    # upload tweet
+    rdb.set("tweet:{tweet_id}", json.dumps(tweet))
 
     # add tweet to user's user timeline
-    rdb.lpush(f"{user}:user_timeline", tweet)
+    rdb.lpush(f"user_timeline:{user}", tweet_id)
 
-    # add tweet to followers' home timelines
-    followers = rdb.smembers(f"{user}:followers")
-    for follower in followers:
-        rdb.lpush(f"{follower}:timeline", tweet)
+    # add tweet to each follower's home timeline
+    for follower in tweet["recipients"]:
+        rdb.lpush(f"home_timeline:{follower}", tweet_id)
 
-    # execute transaction
-    pipe.execute()
-    return f"Tweet sent from {user}"
+    # track hashtags and mentions
+    for hashtag in tweet["hashtags"]:
+        rdb.sadd(f"hashtags:{hashtag}", tweet_id)
+
+    for mention in tweet["mentions"]:
+        rdb.sadd(f"mentions:{mention}", tweet_id)
+
+    # todo end transaction
 
 
 # delete tweet
 @app.delete("/tweet/{user}/{tweet_id}")
 async def delete_tweet(user: str, tweet_id: str):
-    # start transaction
-    pipe = rdb.pipeline(transaction=True)
-    pipe.multi()
-
-    # todo
+    # todo start transaction
     # remove tweet from user's user timeline
-    # remove tweet from followers' home timelines
+    rdb.lrem(f"user_timeline:{user}", 0, tweet_id)
 
-    # execute transaction
-    pipe.execute()
-    return {"message": "Tweet deleted"}
+    # get time of sending
+    tweet = json.loads(rdb.get(f"tweet:{tweet_id}"))
+
+    # remove tweet from each recipient's home timeline
+    # unless tweet is too old
+    if time.time() - tweet["time"] < TTL:
+        for recipient in tweet["recipients"]:
+            rdb.lrem(f"home_timeline:{recipient}", 0, tweet_id)
+
+    for hashtag in tweet["hashtags"]:
+        rdb.srem(f"hashtags:{hashtag}", tweet_id)
+
+    for mention in tweet["mentions"]:
+        rdb.srem(f"mentions:{mention}", tweet_id)
+
+    # todo end transaction
 
 
 # show user timeline
 @app.get("/timeline/{user}/user")
 async def show_user_timeline(user: str, skip: int = SKIP_DEFAULT, limit: int = LIMIT_DEFAULT):
-    # get user's user timeline
-    timeline = rdb.lrange(f"{user}:user_timeline", skip, limit - LIMIT_OFFSET)
+    timeline = []
+    # get all tweets from user timeline
+    tweet_ids = rdb.lrange(f"{user}:user_timeline", skip, limit - LIMIT_OFFSET)
+    for tweet_id in tweet_ids:
+        timeline.append(rdb.get(f"tweet:{tweet_id}"))
     return timeline
 
 
 # show home timeline
 @app.get("/timeline/{user}/home")
 async def show_home_timeline(user: str, skip: int = SKIP_DEFAULT, limit: int = LIMIT_DEFAULT):
-    # get user's timeline
-    timeline = rdb.lrange(f"{user}:timeline", skip, limit - LIMIT_OFFSET)
+    timeline = []
+    # get all tweets from home timeline
+    tweet_ids = rdb.lrange(f"{user}:home_timeline", skip, limit - LIMIT_OFFSET)
+    for tweet_id in tweet_ids:
+        timeline.append(rdb.get(f"tweet:{tweet_id}"))
     return timeline
 
 
@@ -121,31 +143,28 @@ async def show_home_timeline(user: str, skip: int = SKIP_DEFAULT, limit: int = L
 @app.post("/follower/{user}/{follower}")
 async def follow_user(user: str, follower: str):
     # add follower to user's followers
-    rdb.sadd(f"{user}:followers", follower)
+    rdb.sadd(f"followers:{user}", follower)
     # ? add user to follower's profile
-
-    return f"{follower} is now following {user}"
 
 
 # unfollow user
 @app.delete("/follower/{user}/{follower}")
 async def unfollow_user(user: str, follower: str):
     # remove follower from user's followers
-    rdb.srem(f"{user}:followers", follower)
+    rdb.srem(f"followers:{user}", follower)
     # ? remove user from follower's profile
-
-    return f"{follower} is no longer following {user}"
 
 
 # show followers
 @app.get("/follower/{user}")
 async def show_followers(user: str):
     # get user's followers
-    followers = rdb.smembers(f"{user}:followers")
+    followers = rdb.smembers(f"followers:{user}")
     return followers
 
 
 # show user profile - GET /user/{user}
 @app.get("/user/{user}")
 async def show_user_profile(user: str):
+    # todo
     return {"user": user}
