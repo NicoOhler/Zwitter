e4e7a72034287268a0a79afc371db65f5aa43b2e
NicoOhler <32089075+NicoOhler@users.noreply.github.com>
2022-09-17 10:49:24 +0200

    Make timeline scrollable

--- frontend/src/twitter/Timeline.svelte
+++ frontend/src/twitter/Timeline.svelte
@@ -3,14 +3,25 @@
 	export let tweets = undefined;
 </script>
 
-<main>
+<main class="scroll">
 	{#if tweets === null}
 		<p>Error fetching tweets</p>
 	{:else if tweets === undefined}
 		<p>loading...</p>
 	{:else if tweets.length > 0}
 		{#each tweets as tweet}
-			<TweetView tweet={tweet} />
+			<TweetView {tweet} />
 		{/each}
 	{/if}
 </main>
+
+<style>
+	.scroll {
+		margin: 3px;
+		padding: 4px;
+		height: 90vh;
+		overflow-x: hidden;
+		overflow-y: auto;
+		text-align: justify;
+	}
+</style>
