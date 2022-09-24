d6caef0f416f7f3403b7a9b34b3e5cfd3b2c0be8
NicoOhler <32089075+NicoOhler@users.noreply.github.com>
2022-09-24 12:26:47 +0200

    Add Google One Tap login

---
+++ frontend/src/logic/Login.svelte
@@ -0,0 +1,28 @@
+<script>
+	const CLIENT_ID = "692257273849-0oicl7ivddep930fh0v2dfrndqh5lbud.apps.googleusercontent.com";
+	export let logged_in = false;
+
+	function onSignIn(googleUser) {
+		// Useful data for your client-side scripts:
+		var profile = googleUser.getBasicProfile();
+		console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+		console.log("Name: " + profile.getName());
+		console.log("Image URL: " + profile.getImageUrl());
+		console.log("Email: " + profile.getEmail());
+
+		// The ID token you need to pass to your backend:
+		var id_token = googleUser.getAuthResponse().id_token;
+		console.log("ID Token: " + id_token);
+
+		logged_in = true;
+	}
+</script>
+
+<svelte:head>
+	<script src="https://accounts.google.com/gsi/client" async defer></script>
+</svelte:head>
+
+<br />Login {logged_in}
+{#if !logged_in}
+	<div id="g_id_onload" data-context="signin" data-client_id={CLIENT_ID} data-callback={onSignIn} data-auto_select="true" />
+{/if}
